df5d0d95924299f5edbc009abbc591b35ed3ea5d
5P4RK3R <mahendransparker@gmail.com>
2023-08-07 21:55:59 +0530

    readme.md updated with dags

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Harvester
+
+## Set Root before each command
+`export AIRFLOW_HOME=$(pwd)/`
+## Initialize the DB
+`airflow db init`
+
+## Create user's credentials
+`airflow users create --username admin --password admin --firstname Mahendra --lastname Gurunathan --role Admin --email mahendragurunathan@gmail.com`
+## Start the airflow webserver
+`airflow webserver --port 8080`
+
+## Start the airflow Scheduler
+`airflow schedular`
+
+## Trigger a DAG
+`airflow dags trigger harvester`
--- remote.py
+++ remote.py
@@ -1,36 +1,3 @@
-from datetime import datetime
-from airflow import DAG
-from airflow.operators.python_operator import PythonOperator
-from airflow.operators.bash_operator import BashOperator
-
-from scrapy.crawler import CrawlerProcess
-import scrapy
-
-class QuotesSpider(scrapy.Spider):
-    name = 'quotes'
-    start_urls = ['http://quotes.toscrape.com']
-
-    def parse(self, response):
-        for quote in response.css('div.quote'):
-            yield {
-                'text': quote.css('span.text::text').get(),
-                'author': quote.css('span small.author::text').get(),
-            }
-        next_page = response.css('li.next a::attr(href)').get()
-        if next_page is not None:
-            yield response.follow(next_page, self.parse)
-
-class MySpider(scrapy.Spider):
-    name = "my_spider"
-    start_urls = ["https://example.com"]
-
-    def parse(self, response):
-        data = {
-            "title": response.css("h1::text").get(),
-            "content": response.css("p::text").getall()
-        }
-        yield data
-
 import scrapy
 
 # Define here the models for your scraped items
@@ -156,38 +123,3 @@ class RemoteSpider(scrapy.Spider):
         il.add_css('raw_job_info','div.job_info_container_sm')
         yield il.load_item()
         # self.logger.info(data)
-
-default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'start_date': datetime(2023, 1, 1),
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 1,
-}
-
-dag = DAG(
-    'harvester',
-    default_args=default_args,
-    schedule_interval='@daily',
-)
-
-# def scrape_quotes():
-#     process = CrawlerProcess(settings={
-#         'USER_AGENT': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
-#         'FEED_FORMAT': 'csv',
-#         'FEED_URI': '/path/to/save/quotes.csv',
-#     })
-#     process.crawl(QuotesSpider)
-#     process.start()
-
-# scrape_task = PythonOperator(
-#     task_id='scrape_quotes',
-#     python_callable=scrape_quotes,
-#     dag=dag,
-# )
-harvi = BashOperator(task_id='harvi',
-    bash_command='scrapy crawl remote -o remote.json',
-    dag=dag,)
-
-harvi
